410e8ab10e9b1dc2a106b883ca9c9d1d193d5404
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-18 14:04:41 +0000

    slight edit
    edit on GamePlay class

--- src/commandline/GamePlay.java
+++ src/commandline/GamePlay.java
@@ -1,5 +1,7 @@
 package commandline;
 
+import java.util.ArrayList;
+
 public class GamePlay {
 	int numOfGamesPlayed; // number of games a player played.
 	int totalScore; 	// total score of each game for a player.
@@ -7,18 +9,20 @@ public class GamePlay {
 	boolean quitGame;	//when user quits the game.
 	boolean veiwStatistics; //when user wants to view statistics of previous games.
 	
-	int maxArraySize = 40;
-	
-	//unshuffledDeck
-	
-	//shuffledDeck
-	//playerDeck
-	//communalPile
-	//currentCardsInRound
 	String selectedCategory;
 	int selectedCategoryValue;
 	
+	int maxArraySize = 40;
 	//Player winnerPlayer = new Player();
+	
+	
+//	private ArrayList<Card> unshuffledDeck = new ArrayList<Card>();
+//	private ArrayList<Card> shuffledDeck = new ArrayList<Card>();
+//	private ArrayList<Card> playerDeck = new ArrayList<Card>();
+//	private ArrayList<Card> communalPile = new ArrayList<Card>();
+//	private ArrayList<Card> currentCardsInRound = new ArrayList<Card>();
+
+	
 	
 	
 }
