9ab7654b0e8b3d4af3c8edaff578c628f139d617
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-18 13:51:56 +0000

    GamePlay Class
    created GamePlay class and started adding some attribute for now.

---
+++ src/commandline/GamePlay.java
@@ -0,0 +1,24 @@
+package commandline;
+
+public class GamePlay {
+	int numOfGamesPlayed; // number of games a player played.
+	int totalScore; 	// total score of each game for a player.
+	boolean playNewGame; // when user select to play again/new game
+	boolean quitGame;	//when user quits the game.
+	boolean veiwStatistics; //when user wants to view statistics of previous games.
+	
+	int maxArraySize = 40;
+	
+	//unshuffledDeck
+	
+	//shuffledDeck
+	//playerDeck
+	//communalPile
+	//currentCardsInRound
+	String selectedCategory;
+	int selectedCategoryValue;
+	
+	//Player winnerPlayer = new Player();
+	
+	
+}
