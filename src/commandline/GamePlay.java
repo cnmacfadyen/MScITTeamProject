16d2e8ca05be79dedd1e1569be812660f6bfede4
Jessica Fong <2138525F@student.gla.ac.uk>
2019-01-18 17:34:33 +0000

    commit to resolve conflict

---
+++ src/commandline/GamePlay.java
@@ -0,0 +1,27 @@
+package commandline;
+
+import java.util.ArrayList;
+
+public class GamePlay {
+	int numOfGamesPlayed; // number of games a player played.
+	int totalScore; 	// total score of each game for a player.
+	boolean playNewGame; // when user select to play again/new game
+	boolean quitGame;	//when user quits the game.
+	boolean veiwStatistics; //when user wants to view statistics of previous games.
+	
+	String selectedCategory;
+	int selectedCategoryValue;
+	
+	int maxSize = 40;
+	//Player winnerPlayer = new Player();
+	
+//	private ArrayList<Card> unshuffledDeck = new ArrayList<Card>(maxSize);
+//	private ArrayList<Card> shuffledDeck = new ArrayList<Card>(maxSize);
+//	private ArrayList<Card> playerDeck = new ArrayList<Card>(maxSize);
+//	private ArrayList<Card> communalPile = new ArrayList<Card>(maxSize);
+//	private ArrayList<Card> currentCardsInRound = new ArrayList<Card>(maxSize);
+
+	
+	
+	
+}
