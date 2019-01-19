74d0682aef279641e8a95328928ade542318c15f
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-19 16:47:13 +0000

    Created class and added methods to existing class
    Created Player.java class for testing.
    added methods to the GamePlay.java class, but they are still not finalized and not finished yet.

--- src/commandline/GamePlay.java
+++ src/commandline/GamePlay.java
@@ -1,13 +1,18 @@
 package commandline;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GamePlay {
-	int numOfGamesPlayed; // number of games a player played.
+	int numOfGamesPlayed =0; // number of games a player played.
 	int totalScore; 	// total score of each game for a player.
+	// in player class we'll need score count for each player.
 	boolean playNewGame; // when user select to play again/new game
 	boolean quitGame;	//when user quits the game.
-	boolean veiwStatistics; //when user wants to view statistics of previous games.
+	//boolean veiwStatistics; //when user wants to view statistics of previous games.
+	
+	Random random = new Random();
 	
 	String selectedCategory;
 	int selectedCategoryValue;
@@ -15,13 +20,159 @@ public class GamePlay {
 	int maxSize = 40;
 	//Player winnerPlayer = new Player();
 	
-//	private ArrayList<Card> unshuffledDeck = new ArrayList<Card>(maxSize);
-//	private ArrayList<Card> shuffledDeck = new ArrayList<Card>(maxSize);
-//	private ArrayList<Card> playerDeck = new ArrayList<Card>(maxSize);
-//	private ArrayList<Card> communalPile = new ArrayList<Card>(maxSize);
-//	private ArrayList<Card> currentCardsInRound = new ArrayList<Card>(maxSize);
+	private ArrayList<Card> unshuffledDeck = new ArrayList<Card>(maxSize);
+	private ArrayList<Card> shuffledDeck = new ArrayList<Card>(maxSize);
+	private ArrayList<Card> playerDeck = new ArrayList<Card>(maxSize);
+	private ArrayList<Card> communalPile = new ArrayList<Card>(maxSize);
+	private ArrayList<Card> currentCardsInRound = new ArrayList<Card>(maxSize);
+	private ArrayList<Player> players = new ArrayList<Player>(5);
+	
+	public void getNumberOfGames() {
+		
+		this.numOfGamesPlayed++;
+	}
+	
+	//viewStatisticsOrPlay();
+	//ask the user to type 1 for viewing statistics of previous games. or 2 for starting a new game.
+	public void selectOption(Scanner in)
+	{
+		int inputForOption = in.nextInt();
+		if(inputForOption == 1) {
+			viewStatistics(true);	
+		}
+		else if(inputForOption == 2) {
+			playNewGame();
+		}
+		else {
+			System.out.println("Your input is incorrect");
+		}
+	}
+	
+	
+	public void playNewGame() {
+		//start new game
+	}
+	
+	// if user typed "q" or "Q" for input then the game is terminated. data is saved???
+	//we can also put this into the selectOption method as another condition for input.
+	public 	void quit(Scanner in) {
+		String inputForQ = in.next();
+		if(inputForQ == "q" || inputForQ == "Q") {
+			System.exit(1);
+		}
+	}
+	
+	public void viewStatistics(boolean selectView) {
+		//connect to database for statistics ??
+	}
+	
+	
+	public void getWinner(Player p) {
+		
+		
+		
+	}
+	
+	//should it return the object element from the players arraylist or
+	//better return the indext of the player element??
+	public int selectStartingPlayer() {
+		int selectRandomPlayer = random.nextInt(5);
+		Object startPlayer = null;
+		int indx =0;
+			//get player 1 and set current player to player 1  (Human Player)
+			if(selectRandomPlayer == 0) {
+				startPlayer = players.get(0);
+				indx = players.indexOf(startPlayer);
+			}
+			//get player 2 and set current player to (AI player 1)
+			else if(selectRandomPlayer == 1) {
+				startPlayer = players.get(1);
+				indx = players.indexOf(startPlayer);
+			}
+			//get player 3 and set current player to (AI player 2)
+			else if(selectRandomPlayer == 2) {
+				startPlayer = players.get(2);
+				indx = players.indexOf(startPlayer);
+			}
+			//get player 4 and set current player to (AI player 3)
+			else if(selectRandomPlayer == 3) {
+				startPlayer = players.get(3);
+				indx = players.indexOf(startPlayer);
+			}
+			//get player 5 and set current player to (AI player 4)
+			else if(selectRandomPlayer == 4) {
+				startPlayer = players.get(4);
+				indx = players.indexOf(startPlayer);
+			}
+		
+		//return startPlayer;
+		return indx;
+		
+	}
+
+	public void selectCategory() {
+		
+		int selectRandomCategory = random.nextInt(5);
+		Scanner in = new Scanner(System.in);
+		int indx = in.nextInt();  // to select a category from 1 to 5.
+		int selectedCat =0;
+		//human player
+		if(selectStartingPlayer() == 0) {
+			selectedCat = indx;
+		}
+		//AI player 1
+		else if(selectStartingPlayer() == 1) {
+			selectedCat = selectRandomCategory;
+		}
+		//AI player 2
+		else if(selectStartingPlayer() == 2) {
+			selectedCat = selectRandomCategory;
+		}
+		//AI player 3
+		else if(selectStartingPlayer() == 3) {
+			selectedCat = selectRandomCategory;
+		}
+		//AI player 4
+		else if(selectStartingPlayer() == 4) {
+			selectedCat = selectRandomCategory;
+		}
+	}
 
 	
+	public void addToCommunalPile() {
+		
+	}
 	
+	public void playRound() {
+		
+	}
+	
+	public void viewCardData(Player p)
+	{
+		
+	}
+	
+	public void getHighestValue() {
+		
+	}
+	//will be needed for keeping track of stats
+	public void getCount() {
+		
+	}
+	
+	
+	public void getRoundNumber() {
+		
+	}
+	
+
+	public void getPlayerNumber() {
+		
+	}
+
+	public void viewWinningCard() {
+		
+	}
+
 	
 }
