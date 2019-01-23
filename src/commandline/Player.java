2618511af18f28cc026a82b1db3863b974e339d7
Jessica Fong <2138525F@student.gla.ac.uk>
2019-01-23 15:53:20 +0000

    overallgame winner and elimination method draft

--- src/commandline/Player.java
+++ src/commandline/Player.java
@@ -1,5 +1,33 @@
 package commandline;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	public boolean gameWinner(ArrayList<Player> p) {
+		if (p.size() >= 2) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	public boolean removeLoser(ArrayList<Integer> p) {
+		boolean lost = true;
+		if (lost) {
+			for (int i = 0; i < p.size(); i++) {
+				if (p.get(i) == 0) {
+					p.remove(i);
+					return true;
+				}
+
+			}
+		}
+		/*
+		 * in game play put this in if (Player.gameWinner(){ arraylist.get(0).getName();
+		 * - to get the winner })
+		 * 
+		 */
+		return false;
+	}
 }
