b113c7e8e7fad781e13e51b700904301dd5fc570
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-26 16:33:56 +0000

    CLModeTest.java edit

--- src/commandline/CLModeTest.java
+++ src/commandline/CLModeTest.java
@@ -1,17 +1,22 @@
 package commandline;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertNotNull;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class CLModeTest {
+	private ArrayList<Card> communalPile = new ArrayList<Card>();
+	private ArrayList<Card> currentCardsInRound = new ArrayList<Card>();
+
 	
-	
+	@Before
+	public void init() {
+		
+	}
 
 	@Test
 	public void testStartingPlayerSelection() {
@@ -31,6 +36,7 @@ class CLModeTest {
 	
 	@Test
 	public void testAddToCommunalPile() {
+
 		
 	}
 	
@@ -44,24 +50,21 @@ class CLModeTest {
 		
 	}
 	
-	
-	@Test
-	public void testOptionSelectionForHuman() {
-		
-	}
-	
 	@Test
 	public void testSelectingOptionForHumanPlayer() {
-		
+//		System.out.println("select an option test: ");
+//		Scanner in = new Scanner(System.in);
+//		PlayGame.selectOption(in);
+//		assertTrue(true);
 	}
 	
 	
 	@Test
 	public void testQuitGameOption() {
-//		Scanner in = new Scanner(System.in);
-//		String s = "q";
-//		PlayGame.selectOption(in);
-		//assertEquals(s );
+		System.out.println("this is a test for quit game option");
+		//Scanner in = new Scanner(System.in);
+	
+		//assertEquals(System.exit(1), PlayGame.quit(in));
 	}
 	
 	
