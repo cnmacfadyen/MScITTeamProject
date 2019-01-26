530404f1b4a04e2cf4bc5c34c4d52839e7ab1d10
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-26 12:51:28 +0000

    CLModeTest.java class
    I added the CLModeTest.java class for the command line mode testing.

---
+++ src/commandline/CLModeTest.java
@@ -0,0 +1,91 @@
+package commandline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CLModeTest {
+	
+	@Test
+	public void testCategorySelection() {
+	
+	}
+
+	@Test
+	public void testStartingPlayerSelection() {
+		
+	}
+	
+	@Test
+	public void testGetHighestValue() {
+		
+	}
+	
+	@Test
+	public void testGetDuplicated() {
+		
+	}
+	
+	
+	@Test
+	public void testAddToCommunalPile() {
+		
+	}
+	
+	@Test
+	public void testRemoveFromCommunalPile() {
+		
+	}
+	
+	@Test
+	public void testWinnerToPlayNextRound() {
+		
+	}
+	
+	
+	@Test
+	public void testOptionSelectionForHuman() {
+		
+	}
+	
+	@Test
+	public void testSelectingOptionForHumanPlayer() {
+		
+	}
+	
+	
+	@Test
+	public void testQuitGame() {
+		
+	}
+	
+	
+	
+	@Test
+	public void testPlayNewGame() {
+		
+	}
+	
+	
+	@Test
+	public void testViewStatistics() {
+		
+	}
+	
+	@Test
+	public void testForExceptions() {
+		
+	}
+	
+	@Test
+	public void testSelectingCategoryForAIPlayer() {
+		
+	}
+	
+	@Test
+	public void testShufflingCards() {
+		
+	}
+
+	
+}
