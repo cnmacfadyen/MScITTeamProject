1311798e7376156c7f4ab31384d25b77b0cd0620
Shaikha Al-Mashaykhi <salmas7@lsu.edu>
2019-01-26 14:52:06 +0000

    CLModeTest.java edit

--- src/commandline/CLModeTest.java
+++ src/commandline/CLModeTest.java
@@ -1,15 +1,17 @@
 package commandline;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
 class CLModeTest {
 	
-	@Test
-	public void testCategorySelection() {
 	
-	}
 
 	@Test
 	public void testStartingPlayerSelection() {
@@ -55,31 +57,43 @@ class CLModeTest {
 	
 	
 	@Test
-	public void testQuitGame() {
-		
+	public void testQuitGameOption() {
+//		Scanner in = new Scanner(System.in);
+//		String s = "q";
+//		PlayGame.selectOption(in);
+		//assertEquals(s );
 	}
 	
 	
 	
 	@Test
-	public void testPlayNewGame() {
-		
+	public void testPlayNewGameOption() {
+		//Scanner in = new Scanner(System.in);
+		//assertEquals(2, PlayGame.selectOption(in));
+
 	}
 	
 	
 	@Test
-	public void testViewStatistics() {
+	public void testViewStatisticsOption() {
 		
 	}
 	
 	@Test
-	public void testForExceptions() {
-		
+	public void testCategorySelection() {
+//		PlayGame pg = new PlayGame();
+//		int sC = pg.selectCategory();
+//
+//		System.out.println("the category you selected is : " + sC);
+//		assertEquals(sC, sC);
+
 	}
 	
 	@Test
 	public void testSelectingCategoryForAIPlayer() {
-		
+		Random random = new Random(5);
+		//PlayGame.selectCategory();
+		assertNotNull(PlayGame.selectCategory());
 	}
 	
 	@Test
