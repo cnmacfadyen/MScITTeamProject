a77ad1f6aa48a99f12118a63fdb06e10d7946641
Caitlin MacFadyen <caitlinmacfadyen@gmail.com>
2019-02-13 19:22:08 +0000

    fixing database issues

--- src/commandline/Database.java
+++ src/commandline/Database.java
@@ -1,5 +1,4 @@
 package commandline;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -12,32 +11,53 @@ public class Database {
 	Statement stmt = null;
 	private static int totalGames;
 	private static int highestRounds;
-	private int totalAverageDraws;
+	private  double totalAverageDraws;
 	private static int computerWon;
 	private static int humanWon;
-
+//	private String dbName = "m_18_0602452b";
 	private String dbName = "postgres";
 
+	private String dbPassword = "DTA123";
+//	private String dbPassword = "2138525f";
 //	private String dbPassword = "0602452b";
-//	private String dbPassword = "DTA123";
 
-	private String dbPassword = "2138525f";
 
-	/**
-	 * Constructor for the database class, ocreateTable() is instantiated along with
-	 * the instance of Database
-	 */
+	
 	public Database() {
 		createTable();
 	}
-
+	
 	/**
-	 * Display the results of: overall total games played, total amount of times a
-	 * computer has won a game, total amount of times a human has won a game, the
-	 * total average draws for overall games and the higher number of rounds in a
-	 * game onto the console, if user selects to view statistics.
 	 * 
+	 * @return If we are able to connect then return the connection. However if unable to connect then return null
 	 */
+	public Connection getConnection() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			//c = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/", dbName,dbPassword);
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", dbName, dbPassword);
+			return c;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public int getAverageDraws() {
+		return (int) totalAverageDraws;
+	}
+	
+	public int setAverageDraws(int avg) {
+		return (int) (totalAverageDraws =  avg);
+	}
+	
+	public int setTotalGames(int tgame) {
+		return totalGames = tgame;
+	}
+	
+	public int getTotalGames() {
+		return totalGames;
+	}
 	public void displayResults() {
 		System.out.println("Game statistics: ");
 		System.out.println("Number of Games: " + totalGames());
@@ -48,12 +68,6 @@ public class Database {
 		connectionClosed();
 	}
 
-	/**
-	 * Connect to the database and make a query to get the total amount of games
-	 * played in the gameresults database
-	 * 
-	 * @return Total amount of games played overall {@link Database#totalGames}
-	 */
 	public int totalGames() {
 		Statement statement = null;
 		try {
@@ -69,19 +83,13 @@ public class Database {
 		return totalGames;
 	}
 
-	/**
-	 * Connect to the database and make a query to get the total amount of games the
-	 * computer has won over the human player in the gameresults database
-	 * 
-	 * @return The number of games the AI has won over the human player
-	 *         {@link Database#computerWon}
-	 */
 	public int computerWon() {
 		Statement statement = null;
 		try {
 			Connection c = getConnection();
 			statement = c.createStatement();
-			ResultSet result = statement.executeQuery("SELECT COUNT(whowon) FROM gameresults WHERE NOT whowon=1");
+			ResultSet result = statement
+					.executeQuery("SELECT COUNT(whowon) FROM gameresults WHERE NOT whowon=1");
 			while (result.next()) {
 				computerWon = result.getInt("count");
 			}
@@ -91,12 +99,6 @@ public class Database {
 		return computerWon;
 	}
 
-	/**
-	 * Connect to the database and make a query to get the total amount of games the
-	 * human player has won over the computer in the gameresults database
-	 * 
-	 * @return Total games the human has won {@link Database#humanWon}
-	 */
 	public int humanWon() {
 		Statement statement = null;
 		try {
@@ -112,13 +114,6 @@ public class Database {
 		return humanWon;
 	}
 
-	/**
-	 * Connect to the database and make a query to get the average drawsm of the
-	 * total amount of games played in the gameresults database
-	 * 
-	 * @return Average draws of total games played
-	 *         {@link Database#totalAverageDraws}
-	 */
 	public double avgDraws() {
 		Statement statement = null;
 		try {
@@ -134,13 +129,6 @@ public class Database {
 		return totalAverageDraws;
 	}
 
-	/**
-	 * Connect to the database and make a query to get the highest number of rounds
-	 * played in a game in the gameresults database
-	 * 
-	 * @return Highest number of rounds in a game played
-	 *         {@link Database#highestRounds}
-	 */
 	public int highestRoundsInAGame() {
 		Statement statement = null;
 		try {
@@ -156,25 +144,12 @@ public class Database {
 		return highestRounds;
 	}
 
-	/**
-	 * Connect to the database and insert results from the TopTrumps game into the
-	 * gameresults database
-	 * 
-	 * @param drawsPerGame       The number of draws in a game
-	 * @param winner             The overall winner in a game played
-	 * @param totalRoundsInAGame The total amount of rounds played in a game
-	 * @param humanRounds        The number of rounds the human has won in a game
-	 * @param ai1                The number of rounds AI player 1 has won in a game
-	 * @param ai2                The number of rounds AI player 2 has won in a game
-	 * @param ai3                The number of rounds AI player 3 has won in a game
-	 * @param ai4                The number of rounds AI player 4 has won in a game
-	 */
-	public void postResultsToDatabase(int drawsPerGame, int winner, int totalRoundsInAGame, int humanRounds, int ai1,
+	public void postResultsToDatabase( int drawsPerGame, int winner, int totalRoundsInAGame, int humanRounds, int ai1,
 			int ai2, int ai3, int ai4) {
 		try {
 			Connection c = getConnection();
 			PreparedStatement create = c.prepareStatement(
-					"INSERT INTO gameresults(drawrounds,whowon,roundsingame,userrounds, ai1rounds,ai2rounds,ai3rounds,ai4rounds) "
+					"INSERT INTO gameresults( drawrounds,whowon,roundsingame,userrounds, ai1rounds,ai2rounds,ai3rounds,ai4rounds) "
 							+ "VALUES(" + drawsPerGame + "," + winner + "," + totalRoundsInAGame + "," + humanRounds
 							+ "," + ai1 + "," + ai2 + "," + ai3 + "," + ai4 + ")");
 
@@ -186,15 +161,11 @@ public class Database {
 		}
 	}
 
-	/**
-	 * Try and connect to the database and if there does not exist a gameresults
-	 * table for TopTrumps then create table
-	 */
 	public void createTable() {
 		try {
 			Connection c = getConnection();
 			PreparedStatement create = c.prepareStatement(
-					"CREATE TABLE IF NOT EXISTS gameresults(GameNumber INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY , "
+					"CREATE TABLE IF NOT EXISTS gameresults(GameNumber INT, "
 							+ "DrawRounds INT, WhoWon INT, RoundsInGame INT, UserRounds INT, AI1Rounds INT, AI2Rounds INT, AI3Rounds INT, AI4Rounds INT)");
 			create.executeUpdate();
 
@@ -203,42 +174,26 @@ public class Database {
 		}
 	}
 
-	/**
-	 * Try and connect to the database
-	 * 
-	 * @return If we are able to connect then return the connection. However if
-	 *         unable to connect then return null
-	 */
-	public Connection getConnection() {
-		try {
-			Class.forName("org.postgresql.Driver");
-			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", dbName, dbPassword);
-			return c;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 
-	/**
-	 * Delete the row from the TopTrumps game results database
+	
+	/**Delete the row from the TopTrumps game results database
 	 * 
 	 * @param num the row to be deleted
 	 */
 	public void deleteRow(int num) {
 		try {
 			Connection c = getConnection();
-			PreparedStatement create = c.prepareStatement("DELETE FROM gameresults WHERE gameNumber=" + num);
+			PreparedStatement create = c.prepareStatement(
+					"DELETE FROM gameresults WHERE gameNumber="+ num);
 			create.executeUpdate();
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
-	 * Closes the connection connecting to the database and if there is a
-	 * SQLException then catch it and print out "Unable to close connection"
+	 * Closes the connection connecting to the database and if there is a SQLException then catch it and print out "Unable to close connection"
 	 */
 	public void connectionClosed() {
 		try {
--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -30,23 +30,24 @@ public class TopTrumpsCLIApplication {
 //			
 //			userWantsToQuit=true; // use this when the user wants to exit the game
 		
-			PlayGame game = new PlayGame();
-			game.openApplication();
-			while (game.keepPlaying) {
-			if (game.start == true) {
-					game.playRemainingRounds();					
-				if(game.gameOver == true) {
-					System.out.println("The winner of the game is: " + game.winnerPlayer.getPlayerName());
-					game.db.postResultsToDatabase(game.totalDrawRounds, game.winnerPlayer.getPlayerNumber(), game.totalRounds, game.p1.getWonRound(), game.p2.getWonRound(),
-							game.p3.getWonRound(), game.p4.getWonRound(), game.p5.getWonRound());
-					if(writeGameLogsToFile == true) {
-						FileWriterClass f = new FileWriterClass();
-						f.returnTrumpsLog();
-					}
-					game.start = false;
-					System.out.println("Press 1 to view statistics, 2 to play a game. Press q at any time to quit.");
-					Scanner s = new Scanner(System.in);
-					game.selectOption(s);
+		PlayGame game = new PlayGame();
+		  
+		   game.openApplication();
+		   while (game.keepPlaying) {
+		   if (game.start == true) {
+		     game.playRemainingRounds();     
+		    if(game.gameOver == true) {
+		     System.out.println("The winner of the game is: " + game.winnerPlayer.getPlayerName());
+		     game.db.postResultsToDatabase(game.totalDrawRounds, game.winnerPlayer.getPlayerNumber(), game.totalRounds, game.p1.getWonRound(), game.p2.getWonRound(),
+		       game.p3.getWonRound(), game.p4.getWonRound(), game.p5.getWonRound());
+		     if(writeGameLogsToFile == true) {
+		      FileWriterClass f = new FileWriterClass();
+		      f.returnTrumpsLog();
+		     }
+		     game.start = false;
+		     System.out.println("Press 1 to view statistics, 2 to play a game. Press q at any time to quit.");
+		     Scanner s = new Scanner(System.in);
+		     game.selectOption(s);
 
 				}
 			}
